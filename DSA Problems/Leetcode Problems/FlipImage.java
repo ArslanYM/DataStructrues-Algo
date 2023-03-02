00f868cdff8f58173b8bfad4d7e7073304ede9e3
ArslanYM <arsalanyaqmalik@gmail.com>
2023-03-02 19:19:24 +0530

    GoogleProblem on Leetcode using bitwise operators

---
+++ DSA Problems/Leetcode Problems/FlipImage.java
@@ -0,0 +1,16 @@
+public class FlipImage {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int[] row : image) {
+            //reverse
+            for(int i = 0; i< (image[0].length+1)/2;i++) {
+                int temp = row[i]^1;
+                row[i] = row[image[0].length-i-1]^1;
+                row[image[0].length-i-1]=temp;
+            }
+
+        }
+        return image;
+
+    }
+
+}
